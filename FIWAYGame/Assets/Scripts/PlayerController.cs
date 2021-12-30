3b461dde7efdd6c4568d4dba81b32d410b9adc08
Saba Rayehi <S.Rayehi@gmail.com>
2021-12-30 12:00:40 +0330

    Add actions for control player

--- FIWAYGame/Assets/Scripts/PlayerController.cs
+++ FIWAYGame/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,27 @@ public class PlayerController : MonoBehaviour
 
 {
 
-    public float factor = 0.01f;
+    public float factor = 0.09f;
     public float jumpAmount = 0.5f;
     public Rigidbody2D rb;
 
     public SpriteRenderer spriteRenderer;
     private bool Jump;
     private Vector3 moveVector;
+    public int[] arr;
+     public Stack<int> myStack = new Stack<int>();
 
     void Start()
     {
 
         Jump = true;
         moveVector = new Vector3(1 * factor, 0, 0);
+        int[] arr = new int[] { 0, 0, 1 };
+       
+        myStack.Push(0);
+        myStack.Push(1);
+        myStack.Push(0);
+        myStack.Push(1);
 
     }
 
@@ -33,24 +41,63 @@ public class PlayerController : MonoBehaviour
             transform.position += moveVector;
 
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
-         
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
-           
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+       {
+            pleyerActions(myStack);
+
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && Jump)
-        {
-            rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
-          
+       if (Input.GetKeyDown(KeyCode.DownArrow))
+       {
+            pleyerActions(myStack);
+
         }
+
+       if (Input.GetKeyDown(KeyCode.Space) && Jump)
+       {
+            pleyerActions(myStack);
+        }
+      
+       
+
+
+
     }
-}
 
 
-   
+
+    private void pleyerActions(Stack<int> stackActions)
+    {
+      
+         Debug.Log("stackActions");
+      
+        if (stackActions.Count > 0)
+        {
+            var p_actions = stackActions.Pop();
+
+            Debug.Log("currentActions " + p_actions);
+            
+            if (p_actions == 0)
+            {
+               
+                rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
+
+            }
+    
+            else if (p_actions == 1)
+            {
+                rb.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
+
+
+            }
+
+
+        }
+
+    }
+
+    }
+
+
+
+    
