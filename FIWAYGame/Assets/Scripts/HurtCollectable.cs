12f6f372f1f6428e79a231eb0f58c4e20e280b45
SanaRayehi <sana.rayehi@gmail.com>
2022-01-23 18:48:14 +0330

    Fix collect star and hurt

---
+++ FIWAYGame/Assets/Scripts/HurtCollectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtCollectable : MonoBehaviour
+{
+    public int LifeToAdd =1;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().ChangeHealth(LifeToAdd);
+            gameObject.SetActive(false);
+        }
+    }
+}
+
