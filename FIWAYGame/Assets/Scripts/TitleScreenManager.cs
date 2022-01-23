bb60fcbc7de20bd965482d8d382c50b77d90396c
SanaRayehi <sana.rayehi@gmail.com>
2022-01-23 19:13:31 +0330

    Add start and exit button

---
+++ FIWAYGame/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void _Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
